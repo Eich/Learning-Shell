2f25b626471729225f9420c9531ba4c473d59246
YanQun-Zhu <351639193@qq.com>
2018-05-28 16:41:04 +0800

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -9,3 +9,20 @@ for IP in $IP_LIST;do
     echo "$IP ping is failure"
     fi
 done
+#If the network has jitter, using ping 3 times to test
+IP_LIST="192.168.1.109 192.168.1.110"
+for IP in $IP_LIST;do
+    NUM=1
+    while [ $NUM -le 3 ];do #le it means <=
+        if ping -c 1 $IP &>/dev/null;then
+            echo "$IP ping is Successful"
+            break
+        else
+           # echo "$IP ping is failure"
+           FINAL_COUNT[$NUM]=$IP
+           let NUM++
+           echo "$FINAL_COUNT[$NUM]"
+        fi
+    done
+done
+echo ${FINAL_COUNT[*]}
